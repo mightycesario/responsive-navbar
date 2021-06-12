a5f538312d8012ef91364b5ffa853272fcbc31cc
cmendoza <clrwxb@gmail.com>
2021-06-11 17:45:11 -0700

    testing

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Check");
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const navbarLinks = document.getElementsByClassName("navbar-links")[0]
+
+console.log(toggleButton)
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active")
+})
